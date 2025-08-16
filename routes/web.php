a5e50e5444815718bce2387c566ab474d186db8f
nishimura <reitonishimura1227@gmail.com>
2025-08-16 11:39:33 +0000

    403

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\CompletedTaskController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShoppingListController;
+use App\Http\Controllers\CompletedShoppingListController;
 
 use App\Http\Controllers\Admin\AuthController as AdminAuthController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
@@ -18,43 +17,42 @@ use App\Http\Controllers\Admin\UserController as AdminUserController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// タスク管理システム
+// 買い物リスト
 Route::get('/', [AuthController::class, 'index'])->name('front.index');
 Route::post('/login', [AuthController::class, 'login']);
+// 会員登録
+Route::prefix('/user')->group(function () {
+    Route::get('/register', [UserController::class, 'index'])->name('front.user.register');
+    Route::post('/register', [UserController::class, 'register'])->name('front.user.register.post');
+});
 // 認可処理
 Route::middleware(['auth'])->group(function () {
-
-    Route::prefix('/task')->group(function () {
-    
-        Route::get('/list', [TaskController::class, 'list']);
-        Route::post('/register', [TaskController::class, 'register']);
-        Route::get('/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
-        Route::get('/edit/{task_id}', [TaskController::class, 'edit'])->whereNumber('task_id')->name('edit');
-        Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');       
-        Route::delete('/delete/{task_id}', [TaskController::class, 'delete'])->whereNumber('task_id')->name('delete');
-        Route::post('/complete/{task_id}', [TaskController::class, 'complete'])->whereNumber('task_id')->name('complete');
-        Route::get('/csv/download', [TaskController::class, 'csvDownload']);
+    Route::prefix('/shopping_list')->group(function () {
+        Route::get('/list', [ShoppingListController::class, 'list'])->name('front.list');
+        Route::post('/register', [ShoppingListController::class, 'register']);
+        Route::delete('/delete/{shopping_list_id}', [ShoppingListController::class, 'delete'])->whereNumber('shopping_list_id')->name('delete');
+        Route::post('/complete/{shopping_list_id}', [ShoppingListController::class, 'complete'])->whereNumber('shopping_list_id')->name('complete');
     });
+    // 購入済み「買うもの」一覧
+    Route::get('/completed_shopping_list/list', [CompletedShoppingListController::class, 'list']);
+    // ログアウト
     Route::get('/logout', [AuthController::class, 'logout']);
-    //課題
-    Route::get('/completed_tasks/list', [CompletedTaskController::class, 'list']);
-
 });
+
 // 管理画面
 Route::prefix('/admin')->group(function () {
     Route::get('', [AdminAuthController::class, 'index'])->name('admin.index');
     Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login');
+    // 認可処理
     Route::middleware(['auth:admin'])->group(function () {
         Route::get('/top', [AdminHomeController::class, 'top'])->name('admin.top');
-        Route::get('/logout', [AdminAuthController::class, 'logout']);
         Route::get('/user/list', [AdminUserController::class, 'list'])->name('admin.user.list');
+        // ログアウト
+        Route::get('/logout', [AdminAuthController::class, 'logout']);
     });
 });
-//登録
-Route::get('/user/register', [UserController::class, 'index']);
-Route::post('/user/register/input', [UserController::class, 'register']);
